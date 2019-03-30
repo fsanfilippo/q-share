61a62997104b6f84d2ebfdffdacc490b4b0a1e43
Francis San Filippo <sanflip77@gmail.com>
2019-03-30 00:51:36 -0500

    fixed queue creation problem and refactored

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ wsServer = new WebSocketServer({
 
 wsServer.on('request', function (request) {
   var connection = request.accept(null, request.origin);
-  console.log("NEW CONNECTION!");
+  console.log('NEW CONNECTION!');
 
   connection.on('message', function (message) {
     if (message.type === 'utf8') {
@@ -40,59 +40,32 @@ wsServer.on('request', function (request) {
       let data = utf8Data.data;
       switch (utf8Data.event) {
         case ('create-queue'): {
-          console.log("remote address: " + connection.remoteAddress);
-          let key = data.queueKey;
-          console.log("new queue created: " + key);
-          if (!key) break;
-          let subscribers = new Array();
-          subscribers.push(connection);
-          queues.set(key, { subscribers, songs: new Array() });
-          selectedQueue.set(connection, { key: key, owner: true });
+          console.log('queue created');
+          handleCreateQueueEvent(data, connection);
           break;
         }
         case ('add-to-queue'): {
-          let key = data.queueKey;
-          let song = data.songData;
-          if (!key) break;
-          let subsAndSongs = queues.get(key);
-          if (!subsAndSongs) break;
-          subsAndSongs.songs.push(song);
-          broadcastUpdate(subsAndSongs, key);
-
-          console.log("new songs added to queue!");
+          handleAddToQueueEvent(data, connection);
           break;
         }
         case ('subscribe-to-queue'): {
-          console.log("new subscription added!");
-          let key = data.queueKey;
-          if (!key) break;
-          let subsAndSongs = queues.get(key);
-          if (!subsAndSongs) break;
-          subsAndSongs.subscribers.push(connection);
-          selectedQueue.set(connection, { key: key, owner: false });
-          msgStr = JSON.stringify({ event: "update", data: { selectedQueue: key, songs: subsAndSongs.songs } });
-          connection.send(msgStr);
+          handleSubscribeToQueuEvent(data, connection);
           break;
         }
         case ('delete-queue'): {
-          let key = data.queueKey;
-          if (!key) break;
-          deleteQueue(data.queueKey);
+          handleDeleteQueueEvent(data, connection);
           break;
         }
         case ('update-queue'): {
-          let key = data.queueKey;
-          if (!key) break;
-          let newQueue = data.newQueue;
-          if (!newQueue) break;
-          let subsAndSongs = queues.get(key);
-          if (!subsAndSongs) break;
-          subsAndSongs.songs = newQueue;
-          broadcastUpdate(subsAndSongs, key);
+          handleUpdateQueueEvent(data, connection);
+          break;
+        }
+        case ('reserve-queue'): {
+          handleReserveQueueEvent(data, connection);
           break;
         }
         default: {
-          console.log("unknown event");
+          console.log('Unknown event');
           break;
         }
 
@@ -103,7 +76,7 @@ wsServer.on('request', function (request) {
   connection.on('close', function (connection) {
     let queue = selectedQueue.get(connection);
     if (!queue) return;
-    console.log("CONNECTION CLOSED. The following queue was deleted: " + queue.key);
+    console.log('CONNECTION CLOSED. The following queue was deleted: ' + queue.key);
     if (queue.owned) {
       deleteQueue(queue.key);
     }
@@ -117,17 +90,15 @@ wsServer.on('request', function (request) {
 });
 
 function broadcastUpdate(subsAndSongs, key) {
-  let msgStr = JSON.stringify({ event: "update", data: { selectedQueue: key, songs: subsAndSongs.songs } });
   subsAndSongs.subscribers.forEach(connection => {
-    console.log(connection);
-    console.log("sent update to: " + connection.remoteAddress);
-    connection.send(msgStr)
+    console.log('sent update to: ' + connection.remoteAddress);
+    sendEvent('update', connection, { selectedQueue: key, songs: subsAndSongs.songs });
   });
 }
 
 function broadCastDeletion(subs) {
   subs.forEach(connection => {
-    connection.send({ event: "stale-queue" })
+    connection.send({ event: 'stale-queue' })
   });
 }
 
@@ -146,11 +117,105 @@ function deleteQueue(key) {
   broadCastDeletion(subs, key);
 }
 
+/*
 
-/**
- * End Websocket Setup
- */
+    ~~~~~~~~ MESSAGE EVENTS ~~~~~~~~~
 
+*/
+function handleCreateQueueEvent(data, connection) {
+  let key = data.queueKey;
+
+  //Is there queue key?
+  if (!key) {
+    sendEvent('error', connection, 'Create queue event with emtpy queueKey');
+    return;
+  }
+
+  //Does the queue already exist
+  if (!queues.get(key)) {
+    sendEvent('error', connection, 'You must first reserve the queue');
+    return;
+  }
+
+  //Did somebody reserve it? probably you
+  if (!queues.get(key).reserved) {
+    sendEvent('error', connection, 'Queue is not resereved');
+    return;
+  }
+
+  //Make the queue and add yourself to subscriber list
+  let subscribers = new Array();
+  subscribers.push(connection);
+  queues.set(key, { subscribers, songs: new Array() });
+  selectedQueue.set(connection, { key: key, owner: true });
+}
+
+function handleAddToQueueEvent(data, connection) {
+  let key = data.queueKey;
+  let song = data.songData;
+
+
+  if (!key) {
+    sendEvent('error', connection, 'The queue key was not provided on add to queue');
+    return;
+  }
+
+  let subsAndSongs = queues.get(key);
+  if (!subsAndSongs) return;
+  subsAndSongs.songs.push(song);
+  broadcastUpdate(subsAndSongs, key);
+
+  console.log('new songs added to queue!');
+  break;
+}
+
+function handleSubscribeToQueuEvent(data, connection) {
+  console.log('new subscription added!');
+  let key = data.queueKey;
+  if (!key) return;
+  let subsAndSongs = queues.get(key);
+  if (!subsAndSongs) return;
+  subsAndSongs.subscribers.push(connection);
+  selectedQueue.set(connection, { key: key, owner: false });
+  msgStr = JSON.stringify({ event: 'update', data: { selectedQueue: key, songs: subsAndSongs.songs } });
+  connection.send(msgStr);
+}
+
+
+function handleDeleteQueueEvent(data, connection) {
+  let key = data.queueKey;
+  if (!key) return;
+  deleteQueue(data.queueKey);
+}
+
+function handleUpdateQueueEvent(data, connection) {
+  let key = data.queueKey;
+  if (!key) return;
+  let newQueue = data.newQueue;
+  if (!newQueue) return;
+  let subsAndSongs = queues.get(key);
+  if (!subsAndSongs) return;
+  subsAndSongs.songs = newQueue;
+  broadcastUpdate(subsAndSongs, key);
+}
+
+function handleReserveQueueEvent(data, connection) {
+
+  let key = data.queueKey;
+  if (!key) return;
+  if (queues.get(key)) {
+    sendEvent('error', connection, { success: false });
+    return;
+  }
+  console.log('Reserved a queue');
+  queues.set(key, { reserved: true });
+  sendEvent('success', connection, { success: true });
+
+}
+function sendEvent(event, connection, data) {
+  let msgStr = JSON.stringify({ event, data });
+  connection.send(msgStr);
+}
 
 /**
  * HTTP API
